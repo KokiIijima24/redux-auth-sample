cf2dbb15665ca02bb07930f665180d1731e34b08
KoukiIijima <lle0nonnndebo@gmail.com>
2021-09-07 23:54:40 +0900

    [update]redux-persistを導入。reduxのデータを永続化。

--- src/App.js
+++ src/App.js
@@ -1,40 +1,47 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "./store/";
-import PrivateRoute from "./components/PrivateRoute";
-import GuestRoute from "./components/GuestRoute";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import MyPage from "./pages/MyPage";
+import store, { persistor } from './store/'
+import PrivateRoute from './components/PrivateRoute'
+import GuestRoute from './components/GuestRoute'
+
+import Home from './pages/Home'
+import Login from './pages/Login'
+import MyPage from './pages/MyPage'
 
 function Menu() {
+function logout(){
+  e.preventDefault();
+  console.log('log out')
+  await dispatch(login(username, password));
+}
+
   return (
     <nav>
-      <Link to="/">Home</Link>
-      {" "}|{" "}
-      <Link to="/login">Login</Link>
-      {" "}|{" "}
-      <Link to="/mypage">MyPage</Link>
+      <Link to='/'>Home</Link> | <Link to='/login'>Login</Link> |{' '}
+      <Link to='/mypage'>MyPage</Link>
     </nav>
-  );
+  )
 }
 
 function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
-        <Menu />
-        <Switch>
-          <Route path="/" exact children={<Home />} />
-          <GuestRoute path="/login" children={<Login />} />
-          <PrivateRoute path="/mypage" children={<MyPage />} />
-        </Switch>
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <Menu />
+          <Switch>
+            <Route path='/' exact children={<Home />} />
+            <GuestRoute path='/login' children={<Login />} />
+            <PrivateRoute path='/mypage' children={<MyPage />} />
+          </Switch>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,31 @@
-import { combineReducers } from "redux";
+import { combineReducers, createStore } from "redux";
 import { configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import thunk from 'redux-thunk'
 
 import authReducer from "./auth";
+
+// 永続化の設定
+const persistConfig = {
+  key: 'root', // Storageに保存されるキー名を指定する
+  storage, // 保存先としてlocalStorageがここで設定される
+  whitelist: ['auth'] // Stateは`todos`のみStorageに保存する
+  // blacklist: ['visibilityFilter'] // `visibilityFilter`は保存しない
+}
 
 const reducer = combineReducers({
   auth: authReducer,
 });
 
-const store = configureStore({ reducer });
+// 永続化設定されたReducerとして定義
+const persistedReducer = persistReducer(persistConfig, reducer)
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: [thunk]
+})
+
+export const persistor = persistStore(store)
 
 export default store;
