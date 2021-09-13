ac9d7d6ccdf82bd65622aaf154838682ca0d8505
KoukiIijima <lle0nonnndebo@gmail.com>
2021-09-14 00:27:15 +0900

    [add]ヘッダーにログインユーザーの名前が出力されるようにした

--- src/components/HeaderMenu.js
+++ src/components/HeaderMenu.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 import { Link } from 'react-router-dom'
 import { logout } from '../store/auth'
 
 function HeaderMenu() {
   const dispatch = useDispatch()
+  const user = useSelector(state => state.auth.user)
 
   const signout = async () => {
     await dispatch(logout())
@@ -16,6 +17,7 @@ function HeaderMenu() {
       <Link to='/'>Home</Link> | <Link to='/login'>Login</Link> |{' '}
       <Link to='/mypage'>MyPage</Link>
       <button onClick={(e) => signout(e)}>Logout</button>
+      <Link>{user.displayName}</Link>
     </nav>
   )
 }
