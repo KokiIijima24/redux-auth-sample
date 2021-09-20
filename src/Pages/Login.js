1e23c90529a3df598abea4e345f7664ccf561232
KoukiIijima <lle0nonnndebo@gmail.com>
2021-09-20 11:39:26 +0900

    [fix]warningの修正

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -16,17 +16,17 @@ const Login = () => {
   }
 
   return (
-    <div class='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col'>
+    <div className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col'>
       <form onSubmit={submit}>
-        <div class='mb-4'>
+        <div className='mb-4'>
           <label
-            class='block text-grey-darker text-sm font-bold mb-2'
-            for='username'
+            className='block text-grey-darker text-sm font-bold mb-2'
+            htmlFor='username'
           >
             Username
           </label>
           <input
-            class='shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker'
+            className='shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker'
             id='username'
             type='text'
             placeholder='Username'
@@ -35,32 +35,32 @@ const Login = () => {
             onChange={(e) => setUsername(e.target.value)}
           />
         </div>
-        <div class='mb-6'>
+        <div className='mb-6'>
           <label
-            class='block text-grey-darker text-sm font-bold mb-2'
-            for='password'
+            className='block text-grey-darker text-sm font-bold mb-2'
+            htmlFor='password'
           >
             Password
           </label>
           <input
-            class='shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3'
+            className='shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3'
             id='password'
             type='password'
             placeholder='******************'
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
-          <p class='text-red text-xs italic'>Please choose a password.</p>
+          <p className='text-red text-xs italic'>Please choose a password.</p>
         </div>
-        <div class='flex items-center justify-between'>
+        <div className='flex items-center justify-between'>
           <button
-            class='bg-blue-600 hover:bg-blue-dark text-white font-bold py-2 px-4 rounded'
+            className='bg-blue-600 hover:bg-blue-dark text-white font-bold py-2 px-4 rounded'
             type='submit'
           >
             Sign In
           </button>
           <a
-            class='inline-block align-baseline font-bold text-sm text-blue hover:text-blue-darker hidden'
+            className='inline-block align-baseline font-bold text-sm text-blue hover:text-blue-darker hidden'
             href='#'
           >
             Forgot Password?
